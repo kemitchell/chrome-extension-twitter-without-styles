f8d5f64f73d071ffbd05d7e4cedca7f684bf6a82
Julian Gruber <julian@juliangruber.com>
2017-11-25 15:32:55 +0100

    hide profile card stats

--- onload.js
+++ onload.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
   var style = document.createElement('style')
   style.appendChild(document.createTextNode(
-    '.ProfileTweet-actionCount { display: none; }'
+    '.ProfileTweet-actionCount, .ProfileCardStats-statValue { display: none; }'
   ))
   document.head.appendChild(style)
 })
